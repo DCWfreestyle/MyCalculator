8b69a3100c3c1ee1a3f6daeb54677c765b9618b6
Huang <dcwong09@gmail.com>
2014-09-06 00:57:21 +0800

    1

--- src/Data.java
+++ src/Data.java
@@ -1,9 +1,19 @@
 
 public class Data {
 	
-	public static String str=new String();
-	public static float result=0;
+	public String str=new String();
+	public float result=0;
+	int n=0;
 	public Data(){
 		
+	}
+	public float getresult(){
+		char[] c = str.toCharArray();
+		n=str.length();
+		for(int i=0;i<n;i++){
+			if( c[i]=='\0') break;
+			System.out.println(c[i]);
+		}
+		return result;
 	}
 }
